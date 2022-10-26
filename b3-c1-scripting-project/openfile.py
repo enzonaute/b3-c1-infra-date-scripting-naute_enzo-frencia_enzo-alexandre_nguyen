aa97ca63a93b9a9ca11126939c06b775adf17713
Nguyen Alex <alexandre.nguyen1@epsi.fr>
2022-10-26 11:48:56 +0200

    code affichage python

--- b3-c1-scripting-project/openfile.py
+++ b3-c1-scripting-project/openfile.py
@@ -1,15 +1,25 @@
 import csv 
-with open('conso-annuelles_v1.csv','r', encoding ='latin-1') as open:
+
+in_path = 'conso-annuelles_v1.csv'
+out_path = 'clean-conso.csv'
+
+
+with open(in_path, encoding ='latin-1') as open:
 	file = csv.DictReader(open,delimiter=';')
 	tab=[]
 
 	for ligne in file:
 		print (dict(ligne))
 		tab.append(ligne)
-	print (len(tab))
+
 	long = len(tab)
 	i = 0
 	while i < long:
 		longueur = len(tab[i])
+		print (tab[i])
 		i = i + 1
 	print ("il y a "+str(long)+" lignes avec "+str(longueur)+ " données.")
+	with open(out_path) as clean:
+		clean.write('nonoon')	
+
+
